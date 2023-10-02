c5c6a4dad5bfc209968131a48837f5cfeabc8d40
Owen-Joos-TX <pancakepft@gmail.co>
2023-10-02 12:37:21 -0500

    Adde main files and transition screens

---
+++ main.py
@@ -0,0 +1,36 @@
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+Builder.load_file("QuizPage.kv")
+
+class QuizPageApp(App):
+    def build(self):
+        return QuizManager()
+class QuizManager(ScreenManager):
+    pass
+class Question1Screen(Screen):
+    def answer_question(self,bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "incorrect"
+
+class Question2Screen(Screen):
+    pass
+
+class Question3Screen(Screen):
+    pass
+
+class CorrectScreen(Screen):
+    def answer_question(self,bool):
+        if bool:
+            self.manager.current = "question2"
+
+class IncorrectScreen(Screen):
+    pass
+
+QuizPageApp().run()
+
