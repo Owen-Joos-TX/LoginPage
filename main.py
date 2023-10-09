390fd7263591fa014b6e038ce5d74e85bd3cc79d
PFTpancake <70246391+PFTpancake@users.noreply.github.com>
2023-10-09 16:38:34 -0500

    .

--- main.py
+++ main.py
@@ -1,47 +1,113 @@
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-
-Builder.load_file("QuizPage.kv")
-
-
-class QuizPageApp(App):
-    def build(self):
-        return QuizManager()
+from kivy.properties import ObjectProperty
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from database import DataBase
 
 
-class QuizManager(ScreenManager):
+class CreateAccountWindow(Screen):
+    nameInfo = ObjectProperty(None)
+    emailInfo = ObjectProperty(None)
+    passwordInfo = ObjectProperty(None)
+
+    def submit(self):
+        if self.nameInfo.text != "" and self.emailInfo.text != "" and self.emailInfo.text.count("@") == 1 and self.emailInfo.text.count(".") > 0:
+            if self.password != "":
+                db.add_user(self.emailInfo.text, self.passwordInfo.text, self.nameInfo.text)
+
+                self.reset()
+
+                sm.current = "login"
+            else:
+                invalidForm()
+        else:
+            invalidForm()
+
+    def login(self):
+        self.reset()
+        sm.current = "login"
+
+    def reset(self):
+        self.emailInfo.text = ""
+        self.passwordInfo.text = ""
+        self.nameInfo.text = ""
+
+
+class LoginWindow(Screen):
+    email = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def loginBtn(self):
+        if db.validate(self.email.text, self.password.text):
+            MainWindow.current = self.email.text
+            self.reset()
+            sm.current = "main"
+        else:
+            invalidLogin()
+
+    def createBtn(self):
+        self.reset()
+        sm.current = "create"
+
+    def reset(self):
+        self.email.text = ""
+        self.password.text = ""
+
+
+class MainWindow(Screen):
+    n = ObjectProperty(None)
+    created = ObjectProperty(None)
+    email = ObjectProperty(None)
+    current = ""
+
+    def logOut(self):
+        sm.current = "login"
+
+    def on_enter(self, *args):
+        password, name, created = db.get_user(self.current)
+        self.n.text = "Account Name: " + name
+        self.email.text = "Email: " + self.current
+        self.created.text = "Created On: " + created
+
+
+class WindowManager(ScreenManager):
     pass
 
 
-class Question1Screen(Screen):
-    def answer_question(self, bool):
-        if bool:
-            self.manager.current = "correct"
-        else:
-            self.manager.current = "incorrect"
+def invalidLogin():
+    pop = Popup(title='Invalid Login',
+                  content=Label(text='Invalid username or password.'),
+                  size_hint=(None, None), size=(400, 400))
+    pop.open()
 
 
-class Question2Screen(Screen):
-    def answer_question(self,text):
-        if (text.lower() == "beaver"):
-            self.ids.test.text == "Correct"
-            self.ids.test.text.font_size: 40
-        else:
-           self.ids.test.text == "Correct"
-           self.ids.test.text.font_size: 70
+def invalidForm():
+    pop = Popup(title='Invalid Form',
+                  content=Label(text='Please fill in all inputs with valid information.'),
+                  size_hint=(None, None), size=(400, 400))
+
+    pop.open()
 
 
-class CorrectScreen(Screen):
-    def answer_question(self, bool):
-        if bool:
-            self.manager.current = "question2"
+kv = Builder.load_file("my.kv")
+
+sm = WindowManager()
+db = DataBase("users.txt")
+
+screens = [LoginWindow(name="login"), CreateAccountWindow(name="create"),MainWindow(name="main")]
+for screen in screens:
+    sm.add_widget(screen)
+
+sm.current = "login"
 
 
-class IncorrectScreen(Screen):
-    def answer_question(self, bool):
-        if bool:
-            self.manager.current = "question1"
+class MyMainApp(App):
+    def build(self):
+        return sm
 
 
-QuizPageApp().run()
+if __name__ == "__main__":
+    MyMainApp().run()
